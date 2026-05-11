12a64e17c54a267ec5ed222ea5d545e6b98674de
itz-maeee <balkawademanaswee@gmail.com>
2026-05-11 16:24:59 +0530

    solution from 11 to 20

---
+++ 19.Queries Quality and Percentage.sql
@@ -0,0 +1,7 @@
+SELECT query_name, 
+ROUND(AVG(rating/position),2) as quality, 
+ROUND(AVG(if(rating<3, 1, 0)*100),2) as poor_query_percentage
+
+from Queries
+WHERE query_name IS NOT NULL
+group by query_name
